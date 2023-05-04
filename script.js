01945609bed2df6d559e7bff01be38c6acb78f7c
Pavlo-Korol <korol.work01@gmail.com>
2023-05-04 21:56:10 +0200

    reset

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-const a = +prompt("Write First Number")
-const b = +prompt("Write Second Number")
-const action = prompt("Choose the action")
 
-switch(action){
-case "+": alert(a + b);
-break;
-case "-": alert(a - b);
-break;
-case "*": alert(a * b);
-break;
-case "/": alert(a / b);
-break;
-}
 
