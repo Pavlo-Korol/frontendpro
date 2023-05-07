70efe50f49e1ac1034cdc7d3e7aa35bb9e1dfe75
Pavlo-Korol <korol.work01@gmail.com>
2023-05-07 19:08:08 +0200

    correct

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Age = prompt("Write your year of birth");
 const City = prompt("Write your city");
-const Sport = prompt("Write your favorite team");
+const Sport = prompt("Write your favorite sport")
 
 if (Age == false) {
     alert("You dodn't write your age.");
@@ -8,37 +8,35 @@ if (Age == false) {
     alert(`You will be ${2023 - Age} years old in 2023.`);
 }
 
-switch (City) {
-    case "Kyiv":
-        alert("You live in Ukraine");
-        break;
-    case "London":
-        alert("You live in Great Britain");
-        break;
-    case "Washington":
-        alert("You live in USA");
-        break;
-    case false:
-        alert("You don`t write your city")
-      break;
-    default:
-        alert(`You live in ${City}`);
-        break;
+
+if (!City) {
+  alert("You dodn't write your city");
+} 
+else if (City === "Kyiv") {
+  alert("You live in Ukraine");
+}
+else if (City === "London"){
+  alert("You live in Great Britain");
+} 
+else if (City === "Washington"){
+  alert("You live in USA")
+}
+else {
+  alert('You live in ' + City);
 }
 
-switch (Sport) {
-  case "Football":
-    alert("Liverpool")
-    break;
-  case "Cybersport":
-    alert("Navi")
-    break;
-  case "Basketball":
-    alert("Boston Celtics")
-    break;
-  case false :
-    alert("You don't write your team")
-    break;
-    default: 
-    alert(`Your sport ${Sport}`)
+if (!Sport) {
+  alert("You don`t write a sport")
+} 
+else if(Sport === "Football"){
+  alert("Your goal is become a C.Ronaldo?");
+}
+else if (Sport === "Cybersport"){
+  alert("Your goal is become a S1mple?");
+}
+else if (Sport === "Basketball"){
+  alert("Your goal is become a M.Jordan?")
+}
+else {
+  alert("Your sport is " + Sport)  
 }
