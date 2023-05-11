882e52f1453a3efeed8bec0f2780e22d74a052cd
Pavlo-Korol <korol.work01@gmail.com>
2023-05-11 21:55:06 +0200

    array homework

--- script.js
+++ script.js
@@ -1,61 +1,32 @@
+const length = parseInt(prompt('Write array length'))
+const arr = []
 
-// first task
-for (let i = 20; i < 30 ; i += 0.5 ){
-  console.log(i)
-}
-
-// second task
-const hrn = 27
-let dollar = 10
-for ( dollar ; dollar <=100; dollar +=10 ){
-  console.log(hrn * dollar)
-}
-
-// third task 
-const x = prompt("Write your number");
-for (let i = 1; i <= 100; i++) {
-  if (i * i <= x) {
-    console.log(i);
-  }
-}
-
-// fourth task 
-
-const number = prompt("Write your number");
-
-let check = true;
-
-if (number > 1) {
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-      check = false;
-      break;
+if (length) {
+  for (let i = 0; i < length; i++) {
+    const arrItem= parseInt(prompt('Write array item'))
+    if (arrItem) {
+      arr.push(arrItem);
+    } else {
+      alert('You dont write array item')
     }
   }
 } else {
-  check = false;
+  alert('You dont write array length')
 }
 
-if (check) {
-  console.log(`${number} is true number`);
+console.log(arr)
+
+function compareNumeric(a, b) {
+  return a - b;
+}
+
+arr.sort(compareNumeric)
+console.log(arr)
+
+if (length < 3) {
+  console.log('I cant delete items 2,3,4 because you array so short')
 } else {
-  console.log(`${number} is not true number`);
+  arr.splice(2,4)
 }
 
-
-// Fifth Task 
-
-const num = prompt("Write your number");
-
-function isPowerOfThree(num) {
-  while (num > 1) {
-    num = num / 3;
-  }
-  return num === 1;
-}
-
-if (isPowerOfThree(num)) {
-  console.log(`${num} can be obtained by raising the number 3 to a certain power.`);
-} else {
-  console.log(`${num} CANNOT be obtained by raising the number 3 to some power.`);
-}
+console.log(arr)
