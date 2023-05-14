2c93282a9c5da9ca5e7181b749f80d7c606ed3e4
Pavlo-Korol <korol.work01@gmail.com>
2023-05-14 11:57:15 +0200

    update users-balance

--- script.js
+++ script.js
@@ -55,20 +55,17 @@ let users = [
   }
   ]
   
-let sumBalance = 0
-const telNumber = users.filter(function (user) {
-  const money = parseFloat(user.balance.replace("$","").replace(",",""));
-  return money > 2000;
-}).map(function (user) {
-  return user.phone;
+const filteredNum = []
+users.forEach(function(element) {
+  if (+element.balance.replace("$", "").replace(",", "") >= 2000) {
+    filteredNum.push(element.phone)
+  }
 });
-const usersBalance = users.map(function(element) { const money = parseFloat(element.balance.replace("$","").replace(",","")); 
-return money
-})
 
-for (let i = 0; i < usersBalance.length; i++) {
-  sumBalance += usersBalance[i];
-}
+console.log(filteredNum);
 
-console.log(telNumber)
-console.log('Summ of users balances is : ' + sumBalance.toFixed(1))
+const sumBalance = users.reduce((previousValue, element) => previousValue + +element.balance.replace("$", "").replace(",", ""), 0);
+
+console.log(sumBalance.toFixed(1))
+
+
